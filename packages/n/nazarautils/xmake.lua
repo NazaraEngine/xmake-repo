547932551cd52c1cbdd283697b4dc5802086b155
SirLynix <lynix680@gmail.com>
2022-08-03 18:34:19 +0200

    Update nazarautils

--- packages/n/nazarautils/xmake.lua
+++ packages/n/nazarautils/xmake.lua
@@ -7,7 +7,7 @@ package("nazarautils")
 
 	add_urls("https://github.com/NazaraEngine/NazaraUtils.git")
 
-	add_versions("2022.06.14", "816a4b60050b1f7a793bcdfab2e76abc7b31797e")
+	add_versions("2022.08.03", "014646ca3b026a440b6a7a5d78fab0592faec2a3")
 
 	on_install(function (package)
 		import("package.tools.xmake").install(package)
