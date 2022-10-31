86d7c50ac04470614d310e034db6d48b02844ef1
SirLynix <lynix680@gmail.com>
2022-10-31 22:07:57 +0100

    Update nzsl

--- packages/n/nzsl/xmake.lua
+++ packages/n/nzsl/xmake.lua
@@ -6,7 +6,7 @@ package("nzsl")
 
     add_urls("https://github.com/NazaraEngine/ShaderLang.git")
 
-    add_versions("2022.10.31", "691fa9c48f0e552fcf74ee66840cfc0fc9492327")
+    add_versions("2022.10.31+1", "8bcabd80ef58918004b12b8db57494b16545660b")
 
     add_deps("nazarautils", "fmt")
     add_deps("frozen", "ordered_map", { private = true })
