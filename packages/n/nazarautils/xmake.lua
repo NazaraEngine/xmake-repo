5ad576af496947907a0c91f99af6203d384d66d3
Jérôme Leclercq <lynix680@gmail.com>
2023-08-31 09:46:24 +0200

    Update nazarautils

--- packages/n/nazarautils/xmake.lua
+++ packages/n/nazarautils/xmake.lua
@@ -3,11 +3,12 @@ package("nazarautils")
     set_homepage("https://github.com/NazaraEngine/NazaraUtils")
     set_description("Header-only utility library for Nazara projects")
     set_license("MIT")
-    set_policy("package.strict_compatibility", true)
 
     add_urls("https://github.com/NazaraEngine/NazaraUtils.git")
 
-    add_versions("2023.08.21", "bd53447b52d1e35e0282a6873aa01a1ef5cba2d9")
+    add_versions("2023.08.31", "234a0d1a6269eb435d71d6e23bd1e70440a22f5e")
+
+    set_policy("package.strict_compatibility", true)
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
