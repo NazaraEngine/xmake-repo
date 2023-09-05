defb05d3c8e3706897d3246d1f0f8fa6c28423ec
Arthur Laurent <arthur.laurent4@gmail.com>
2023-09-05 21:44:37 +0200

    Improve nzsl rule (#6)
    * add outputdir support
    
    * add prefixdir support to nzsl compile.shader rule
    
    * use find_tool to get nzslc executable for nzsl compile.shader rule
    
    * revert to use project.required_package for find nzslc
    
    * fix tabs
    
    * Update compile_shaders.lua
    
    ---------
    
    Co-authored-by: Jérôme Leclercq <lynix680@gmail.com>

--- packages/n/nzsl/rules/compile_shaders.lua
+++ packages/n/nzsl/rules/compile_shaders.lua
@@ -2,24 +2,24 @@
 rule("compile.shaders")
 	set_extensions(".nzsl", ".nzslb")
 
-	before_buildcmd_file(function (target, batchcmds, shaderfile, opt)
-		import("core.project.project")
-		import("core.tool.toolchain")
-
-		-- warning: project.required_package is not a stable interface, this may break in the future
-		local nzsl = path.join(project.required_package("nzsl"):installdir(), "bin", "nzslc")
-
-		-- add commands
-		batchcmds:show_progress(opt.progress, "${color.build.object}compiling.shader %s", shaderfile)
-		local argv = { "--compile=nzslb-header", "--partial", "--optimize" }
-
-		-- handle --log-format
-		local kind = target:data("plugin.project.kind") or ""
-		if kind:match("vs") then
-			table.insert(argv, "--log-format=vs")
+	on_config(function(target)
+		-- add outputdir to include path
+		local outputdir = target:extraconf("rules", "compile.shaders", "outputdir") or path.join(target:autogendir(), "rules", "compile.shaders")
+		if not os.isdir(outputdir) then
+			os.mkdir(outputdir)
 		end
+		target:add("includedirs", outputdir)
+	end)
 
-		table.insert(argv, shaderfile)
+	before_buildcmd_file(function (target, batchcmds, shaderfile, opt)
+		import("core.tool.toolchain")
+		import("lib.detect.find_tool")
+
+		local outputdir = target:extraconf("rules", "compile.shaders", "outputdir") or path.join(target:autogendir(), "rules", "compile.shaders")
+		local fileconfig = target:fileconfig(shaderfile)
+		if fileconfig and fileconfig.prefixdir then
+			outputdir = path.join(outputdir, fileconfig.prefixdir)
+		end
 
 		-- on mingw we need run envs because of .dll dependencies which may be not part of the PATH
 		local envs
@@ -30,7 +30,30 @@ rule("compile.shaders")
 			end
 		end
 
-		batchcmds:vrunv(nzsl, argv, { curdir = ".", envs = envs })
+		-- find nzslc
+		local nzsl = project.required_package("nzsl~host") or project.required_package("nzsl")
+		local nzsldir
+		if nzsl then
+			nzsldir = path.join(nzsl:installdir(), "bin")
+		end
+
+		local nzslc = find_tool("nzslc", { paths = nzsldir, envs = envs })
+		assert(nzslc, "nzslc not found! please install nzsl package")
+
+		-- add commands
+		batchcmds:show_progress(opt.progress, "${color.build.object}compiling.shader %s", shaderfile)
+		local argv = { "--compile=nzslb-header", "--partial", "--optimize", "--output=" .. outputdir }
+		batchcmds:mkdir(outputdir)
+
+		-- handle --log-format
+		local kind = target:data("plugin.project.kind") or ""
+		if kind:match("vs") then
+			table.insert(argv, "--log-format=vs")
+		end
+
+		table.insert(argv, shaderfile)
+
+		batchcmds:vrunv(nzslc.program, argv, { curdir = ".", envs = envs })
 
 		local outputFile = path.join(path.directory(shaderfile), path.basename(shaderfile) .. ".nzslb.h")
 
