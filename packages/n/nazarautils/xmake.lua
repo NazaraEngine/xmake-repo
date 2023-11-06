ed228b5259e5f3228507e11a1f1dfdcb62a83a33
SirLynix <lynix680@gmail.com>
2023-11-06 17:26:06 +0100

    Update nazarautils

--- packages/n/nazarautils/xmake.lua
+++ packages/n/nazarautils/xmake.lua
@@ -6,7 +6,7 @@ package("nazarautils")
 
     add_urls("https://github.com/NazaraEngine/NazaraUtils.git")
 
-    add_versions("2023.09.11+1", "c3df3d857dd6348e41d88eb2deba0037355d72d7")
+    add_versions("2023.11.06", "834ce36688a3231f4267a043f3fb9371342ca354")
 
     set_policy("package.strict_compatibility", true)
 
