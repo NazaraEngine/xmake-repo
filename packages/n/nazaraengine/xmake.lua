d822e1881ee041152a864a05dce302264996ed18
Jérôme Leclercq <lynix680@gmail.com>
2024-08-09 13:05:52 +0200

    Update nazaraengine

--- packages/n/nazaraengine/xmake.lua
+++ packages/n/nazaraengine/xmake.lua
@@ -6,7 +6,7 @@ package("nazaraengine")
 
     set_urls("https://github.com/NazaraEngine/NazaraEngine.git")
 
-    add_versions("2024.08.06", "c7216b4eda4d4b2dc08fe1380ae37156111fb75c")
+    add_versions("2024.08.09", "f555c87c765f80c407bd984dcf4427f5cf35f6f3")
 
     add_deps("nazarautils")
 
