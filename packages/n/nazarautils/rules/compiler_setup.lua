8866e09989580e4fa6ba090257fa97f996c53250
Jérôme Leclercq <lynix680@gmail.com>
2026-04-08 23:53:28 +0200

    Add additional compiler warnings for error handling

--- packages/n/nazarautils/rules/compiler_setup.lua
+++ packages/n/nazarautils/rules/compiler_setup.lua
@@ -9,7 +9,9 @@ rule("compiler_setup")
             target:add("cxflags", "/we4062", {tools = "cl"}) -- Switch case not handled (warning as error)
             target:add("cxflags", "/we4426", {tools = "cl"}) -- Optimization flags changed after including header, may be due to #pragma optimize() (warning as error)
             target:add("cxflags", "/we5038", {tools = "cl"}) -- Data member will be initialized after data member (warning as error)
-        
+            target:add("cxflags", "/we4715", {tools = "cl"}) -- not all control paths return a value (warning as error)
+            target:add("cxflags", "/we4716", {tools = "cl"}) -- must return a value (warning as error)
+
             -- Disable the following warnings:
             target:add("cxflags", "/wd4251", {tools = "cl"}) -- class needs to have dll-interface to be used by clients of class blah blah blah
             target:add("cxflags", "/wd4275", {tools = "cl"}) -- DLL-interface class 'class_1' used as base for DLL-interface blah
